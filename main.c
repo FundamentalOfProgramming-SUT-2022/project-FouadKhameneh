7abc73a778db38414c3919814d0cdc76b87cb949
Fouad Khameneh <75120440+FouadKhameneh@users.noreply.github.com>
2023-01-30 14:26:36 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+int main()
+{
+    char cmd[50];
+    scanf("%s",cmd);
+    
+}
